c16a494b2e363c4fe9478d801a5252e3e22aabac
username <i-irianto.perkasa@dana.id>
2022-02-10 12:44:47 +0700

    IOS Hello World

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Irianto Mauduta Perkasa on 10/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
